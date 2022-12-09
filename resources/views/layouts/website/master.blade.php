8c259380a15629e26e2fe33abb2075308a992299
Kareem-Tarek <kareemtarekpk@gmail.com>
2022-12-09 10:13:42 +0200

    ....

--- resources/views/layouts/website/master.blade.php
+++ resources/views/layouts/website/master.blade.php
@@ -32,6 +32,7 @@
     <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="stylesheet" href="/assets/css/demos/demo-15.css">
     <link rel="stylesheet" href="/assets/css/my-custom-css.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
--- resources/views/layouts/website/register-now.blade.php
+++ resources/views/layouts/website/register-now.blade.php
@@ -1,13 +1,13 @@
 <!--------- for guests ONLY! (which are unknown users that are not registered to the DB) ---------->
 @if(!auth()->user() || Auth::guest()) <!---- both conditions means the same thing which is if user is a guest ----->
-        <a href="{{ route('register') }}">
-            <span class="badge register-now-text">
-                Register Now!<br><i class="fa-solid fa-computer-mouse click-here-to-register-now-icon"></i>
-            </span>
-        </a>
+    <a href="{{ route('register') }}">
+        <span class="badge register-now-badge">
+            Register Now!&nbsp;&nbsp;<i class="fa-solid fa-computer-mouse"></i>
+        </span>
+    </a>
 
 <style>
-    .badge{
+    .register-now-badge{
         position: fixed;
         left: 1px;
         top: 16%;
@@ -19,19 +19,15 @@
         padding: 10px 0px 10px 0px;
         border: 0px solid black;
         font-size: 115%;
-        z-index: 5000;
+        z-index: 1000;
         /* box-shadow: 0 1rem 3rem rgba(#000000, 0.1); */
     }
     
-    .badge:hover{
+    .register-now-badge:hover{
         /* background-color: aliceblue; */
         background-color: rgba(178, 179, 180, 0.65);
         color: rgb(78, 78, 80);
         border: 0px solid black;
-    }
-    
-    .click-here-to-register-now-icon{
-        padding-top: 6%;  
     }
 </style>
 @endif
