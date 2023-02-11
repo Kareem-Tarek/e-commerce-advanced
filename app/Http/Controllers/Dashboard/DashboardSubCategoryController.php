18a9e2e69b1c5f5a089dadb455b49e24e18be7d7
Kareem-Tarek <kareemtarekpk@gmail.com>
2023-02-11 05:23:30 +0200

    some modifications done

--- app/Http/Controllers/Dashboard/DashboardSubCategoryController.php
+++ app/Http/Controllers/Dashboard/DashboardSubCategoryController.php
@@ -112,7 +112,13 @@ class DashboardSubCategoryController extends Controller
      */
 
     public function update(Request $request, $id)
-    {      
+    {   
+        // $request->validate([
+        //     'name'        => 'required|string|unique:sub_categories',
+        //     'description' => 'nullable|string',
+        //     'cat_id'      => 'required',
+        // ]);
+
         $sub_categories_old = SubCategory::findOrFail($id);    //this variable is used only to get the old data
         //$category = Category::find($id);
         // if($category->id == $sub_categories->cat_id){
@@ -122,14 +128,14 @@ class DashboardSubCategoryController extends Controller
         $sub_categories = SubCategory::findOrFail($id);
         $sub_categories->update_user_id = auth()->user()->id;
 
-        if($request->name == $sub_categories_old->name && 
+        if($request->name == $sub_categories_old->name &&   //nothing changed in all the columns
         $request->cat_id == $sub_categories_old->cat_id && 
         $request->description == $sub_categories_old->description){
             return redirect()->route('subcategories.index')
             ->with(['updated_same_name_sub_category_message' => "You entered the same values of sub-category, category & description for sub-category ($sub_categories->name). There are no changes made, please try again!"]);
         }
         ///
-        elseif($request->name != $sub_categories_old->name && 
+        elseif($request->name != $sub_categories_old->name &&   //only "name" column is changed!
         $request->cat_id == $sub_categories_old->cat_id && 
         $request->description == $sub_categories_old->description){
             $sub_categories->name        = $request->name;
@@ -139,7 +145,7 @@ class DashboardSubCategoryController extends Controller
             return redirect()->route('subcategories.index')
             ->with(['updated_sub_category_message' => "$sub_categories_old->name → ($sub_categories->name) - Edited successfully!"]);
         }
-        elseif($request->name == $sub_categories_old->name && 
+        elseif($request->name == $sub_categories_old->name &&   //only "cat_id" column is changed!
         $request->cat_id != $sub_categories_old->cat_id && 
         $request->description == $sub_categories_old->description){
             $sub_categories->name        = $sub_categories_old->name;
@@ -151,7 +157,7 @@ class DashboardSubCategoryController extends Controller
             return redirect()->route('subcategories.index')
             ->with(['updated_sub_category_message' => "The sub-category's ($sub_categories->name) category has been changed successfully!"]);
         }
-        elseif($request->name == $sub_categories_old->name && 
+        elseif($request->name == $sub_categories_old->name &&   //only "description" column is changed!
         $request->cat_id == $sub_categories_old->cat_id && 
         $request->description != $sub_categories_old->description){
             $sub_categories->name        = $sub_categories_old->name;
@@ -160,6 +166,36 @@ class DashboardSubCategoryController extends Controller
             $sub_categories->save();
             return redirect()->route('subcategories.index')
             ->with(['updated_sub_category_message' => "The sub-category's ($sub_categories->name) description has been changed successfully!"]);
+        }
+        elseif($request->name != $sub_categories_old->name &&   //only "name" & "description" columns are changed!
+        $request->cat_id == $sub_categories_old->cat_id && 
+        $request->description != $sub_categories_old->description){
+            $sub_categories->name        = $request->name;
+            $sub_categories->cat_id      = $sub_categories_old->cat_id;
+            $sub_categories->description = $request->description;
+            $sub_categories->save();
+            return redirect()->route('subcategories.index')
+            ->with(['updated_sub_category_message' => "The sub-category ($sub_categories_old->name) has been changed to ($sub_categories->name) + its description successfully!"]);
+        }
+        elseif($request->name != $sub_categories_old->name &&   //only "name" & "cat_id" columns are changed!
+        $request->cat_id != $sub_categories_old->cat_id && 
+        $request->description == $sub_categories_old->description){
+            $sub_categories->name        = $request->name;
+            $sub_categories->cat_id      = $request->cat_id;
+            $sub_categories->description = $sub_categories_old->description;
+            $sub_categories->save();
+            return redirect()->route('subcategories.index')
+            ->with(['updated_sub_category_message' => "The sub-category ($sub_categories_old->name) has been changed to ($sub_categories->name) + its category successfully!"]);
+        }
+        elseif($request->name == $sub_categories_old->name &&   //only "description" & "cat_id" columns are changed!
+        $request->cat_id != $sub_categories_old->cat_id && 
+        $request->description != $sub_categories_old->description){
+            $sub_categories->name        = $sub_categories_old->name;
+            $sub_categories->cat_id      = $request->cat_id;
+            $sub_categories->description = $request->description;
+            $sub_categories->save();
+            return redirect()->route('subcategories.index')
+            ->with(['updated_sub_category_message' => "The sub-category's ($sub_categories->name) category & description have been changed successfully!"]);
         }
 
     }
